5d48367c7c7686fd42954ed13eb56c6dd52c1bc1
Yaghoubi-MH <sm.yaghoubi.mh@gmail.com>
2023-03-10 11:44:23 +0330

    refactored table

--- src/components/Table/BodyTable/index.js
+++ src/components/Table/BodyTable/index.js
@@ -2,8 +2,7 @@ import React from "react";
 
 const BodyTable = ({style, data}) => {
     return(
-        <tbody className={style.add_row_after_header} >{
-        }>
+        <tbody className={style.add_row_after_header} >
             { data.map((i, index) => (
                 <tr key={index}>
                     {Object.values(i).map((i, index) => <td  className={style.body_cell_table} key={index}>{i}</td>)}
